b5d63a6a4b69db6c78a7e1b27bec332b240f67ee
fthe <lcslianandfthe.cs@gmail.com>
2024-07-23 23:51:55 +0900

    gosec: fixed all issue

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -93,8 +94,11 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:         ":" + port,
+		Handler:      router,
+		ReadTimeout:  time.Second * 5,
+		IdleTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 5,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
